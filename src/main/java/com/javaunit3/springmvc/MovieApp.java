656cb05f674720e41c45f8dc4a5c63f0a7e48b4e
Denetrah <dntrholloman@gmail.com>
2022-09-03 18:25:05 -0500

    Initial commit

---
+++ src/main/java/com/javaunit3/springmvc/MovieApp.java
@@ -0,0 +1,21 @@
+package com.javaunit3.springmvc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+@ComponentScan
+public class MovieApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(MovieApp.class);
+        BestMovieService bestMovieService = applicationContext.getBean("bestMovieService", BestMovieService.class);
+
+        Movie bestMovie = bestMovieService.getBestMovie();
+
+        System.out.println("Title: " + bestMovie.getTitle());
+        System.out.println("Maturity Rating: " + bestMovie.getMaturityRating());
+        System.out.println("Genre: " + bestMovie.getGenre());
+
+    }
+}
